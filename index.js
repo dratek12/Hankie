b29f3ee439bff3f15b12a5af8a59f882962b210c
dratek12 <58343637+dratek12@users.noreply.github.com>
2023-07-31 20:02:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+// Funkce pro náhodné rozhodnutí, zda písmeno má být velké
+function isUpperCaseRandom() {
+    return Math.random() < 0.5; // 50% pravděpodobnost velkého písmena
+}
+
 document.getElementById('input-form').addEventListener('submit', event => {
     event.preventDefault();
     const textInput = event.target.getElementsByTagName('textarea')[0].value;
     const textArray = textInput.split('');
     let textOutput = '';
     textArray.forEach((character, index) => {
-        if(index % 2 === 0) {
+        if (isUpperCaseRandom()) {
             textOutput += character.toUpperCase();
-        }
-        else {
-            textOutput += character;
+        } else {
+            textOutput += character.toLowerCase();
         }
     });
     document.getElementById('text-output').innerText = textOutput;

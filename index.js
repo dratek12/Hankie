95307a64fc78f912faa185fa6190c5638f33bdb5
dratek12 <58343637+dratek12@users.noreply.github.com>
2024-04-12 18:42:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,36 @@ function isUpperCaseRandom() {
     return Math.random() < 0.50; // 50% pravděpodobnost velkého písmena
 }
 
-document.getElementById('input-form').addEventListener('submit', event => {
-    event.preventDefault();
-    const textInput = event.target.getElementsByTagName('textarea')[0].value;
-    const textArray = textInput.split('');
-    let textOutput = '';
-    textArray.forEach((character, index) => {
-        if (isUpperCaseRandom()) {
-            textOutput += character.toUpperCase();
-        } else {
-            textOutput += character.toLowerCase();
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('input-form').addEventListener('submit', event => {
+        event.preventDefault();
+        const textInput = event.target.getElementsByTagName('textarea')[0].value;
+        const textArray = textInput.split('');
+        let textOutput = '';
+        textArray.forEach((character, index) => {
+            if (isUpperCaseRandom()) {
+                textOutput += character.toUpperCase();
+            } else {
+                textOutput += character.toLowerCase();
+            }
+        });
+        document.getElementById('text-output').innerText = textOutput;
     });
-    document.getElementById('text-output').innerText = textOutput;
+    
+    const textOutput = document.getElementById('text-output');
+    
+    textOutput.addEventListener('click', function() {
+        copyToClipboard(textOutput.textContent);
+    });
+    
+    function copyToClipboard(text) {
+        navigator.clipboard.writeText(text).then(function() {
+            textOutput.textContent = 'Copied!';
+            setTimeout(function() {
+                textOutput.textContent = text;
+            }, 1500);
+        }, function(err) {
+            console.error('Failed to copy: ', err);
+        });
+    }
 });

c3135498087dfd154543e26936889c30368bf33a
dratek12 <58343637+dratek12@users.noreply.github.com>
2024-04-14 16:58:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-// Funkce pro náhodné rozhodnutí, zda písmeno má být velké
-function isUpperCaseRandom() {
-    return Math.random() < 0.50; // 50% pravděpodobnost velkého písmena
-}
-
 document.addEventListener('DOMContentLoaded', function() {
+    // Funkce pro náhodné rozhodnutí, zda písmeno má být velké
+    function isUpperCaseRandom() {
+        const probability = Math.random(); // Náhodné číslo mezi 0 a 1
+        if (probability < 0.50) {
+            return false; // 50% pravděpodobnost malého písmene
+        } else if (probability < 0.90) {
+            return true; // 40% pravděpodobnost velkého písmena (50% - 90%)
+        } else if (probability < 0.95) {
+            return false; // 5% pravděpodobnost malého písmene (90% - 95%)
+        } else {
+            return true; // 5% pravděpodobnost velkého písmena (95% - 100%)
+        }
+    }
+
     document.getElementById('input-form').addEventListener('submit', event => {
         event.preventDefault();
         const textInput = event.target.getElementsByTagName('textarea')[0].value;
@@ -16,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 textOutput += character.toLowerCase();
             }
         });
-        document.getElementById('text-output').innerText = textOutput;
+        document.getElementById('text-output').textContent = textOutput; // Změna použití na textContent
     });
     
     const textOutput = document.getElementById('text-output');
@@ -29,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
         navigator.clipboard.writeText(text).then(function() {
             textOutput.textContent = 'Copied!';
             setTimeout(function() {
-                textOutput.textContent = text;
+                textOutput.textContent = text; // Použití původního textu
             }, 1500);
         }, function(err) {
             console.error('Failed to copy: ', err);
